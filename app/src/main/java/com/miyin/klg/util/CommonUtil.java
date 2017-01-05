5d94b8a28014b7167645e4830014cefe9b287b51
en <enjava@163.com>
2017-01-05 10:03:53 +0800

    en

--- app/src/main/java/com/miyin/klg/util/CommonUtil.java
+++ app/src/main/java/com/miyin/klg/util/CommonUtil.java
@@ -23,7 +23,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListAdapter;
 import android.widget.ListView;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.security.MessageDigest;
